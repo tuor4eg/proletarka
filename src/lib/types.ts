3f3a57f7bd5a7ec45bca81a02ddadf48e5ba1096
tuor4eg <ast@truelab.games>
2026-04-11 15:44:23 +0300

    fix build

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,10 @@
-export type SearchParams = Promise<{ q?: string; sort?: string; letter?: string; page?: string }>
+export type SearchParams = Promise<{
+    q?: string
+    sort?: string
+    letter?: string
+    page?: string
+    backstack?: string
+}>
 
 export type LoginPageProps = {
     searchParams: Promise<{ error?: string }>
